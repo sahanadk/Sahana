c630d14e96d6ddb306b548f15c88b70a2cf3adef
Sahana <Sahana@Sahana-PC>
2019-04-04 10:25:41 -1200

    commit1

---
+++ DataDrivenFramework/src/com/abc/MagentoTest/Magento.java
@@ -0,0 +1,43 @@
+package com.abc.MagentoTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Magento {
+
+	public static void main(String[] args) throws Exception {
+		FileInputStream fis=new FileInputStream("E:\\\\selenium\\DataDrivenFramework\\src\\com\\abc\\Utilities\\data.properties");
+		Properties p=new Properties();
+		
+		p.load(fis);
+		
+		String urldata=p.getProperty("url");
+		String emaildata=p.getProperty("email");
+		String passdata=p.getProperty("password");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.get(urldata);
+		
+		driver.findElement(By.linkText("My Account")).click();
+		
+		driver.findElement(By.id("email")).sendKeys(emaildata);
+		driver.findElement(By.id("pass")).sendKeys(passdata);
+		
+		driver.findElement(By.id("send2")).click();
+		driver.findElement(By.linkText("Log Out")).click();
+		
+		Thread.sleep(3000);
+		
+	}
+
+}
