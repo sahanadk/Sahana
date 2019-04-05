15a62e4899c988d10f97508e11755b2f1e2047dd
Sahana <Sahana@Sahana-PC>
2019-04-04 22:06:16 -1200

    change to firefox driver

--- DataDrivenFramework/src/com/abc/MagentoTest/Magento.java
+++ DataDrivenFramework/src/com/abc/MagentoTest/Magento.java
@@ -23,7 +23,7 @@ public class Magento {
 		String emaildata=p.getProperty("email");
 		String passdata=p.getProperty("password");
 		
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		
